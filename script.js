bfc0f55034c3598e50cc3a0c7cab15a50c0048ca
Muaz <muazislambabar@gmail.com>
2026-07-13 14:11:37 +0500

    This is

---
+++ script.js
@@ -0,0 +1,12 @@
+// console.log("Hello World");
+// let name = "Muaz";
+// console.log(name);
+
+// let firstName = "Muaz";
+// let lastName = "Babar";
+
+// let fullName = firstName.concat(lastName);
+// console.log(fullName);
+
+// let fullNameSecond = `HiI am  ${firstName}  ${lastName}`;
+// console.log(fullNameSecond.trim);
