35d07d29228710109d04431c621960a0d9d7ccb5
Muaz <muazislambabar@gmail.com>
2026-07-13 14:14:21 +0500

    jlksdjfs

--- script.js
+++ script.js
@@ -10,3 +10,25 @@
 
 // let fullNameSecond = `HiI am  ${firstName}  ${lastName}`;
 // console.log(fullNameSecond.trim);
+
+// let p = 23;
+// let o = 323;
+// let i = 44;
+
+// // function sum(a,b,c) {
+// //     return a + b + c
+// // }
+
+// const binomial = (a, b, c) => {
+//   return (a + b) ** 2 + c;
+// };
+// console.log("The sum of three digits is ", binomial(p, o, i));
+
+// const binomialSecondFunction = (a, b, c) => a ** 2 - 2 * a * b + b ** 2 + c;
+
+// console.log("The second binomial is", binomialSecondFunction(p,o,i))
+
+// const show = () => {
+//   alert("button clicked");
+//   confirm("This is website store captcha");
+// };
